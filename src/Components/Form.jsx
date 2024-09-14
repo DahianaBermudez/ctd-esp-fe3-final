b4fced47025532080f27f4605717cd1182d98f33
dahiana bermudez <c.dahianabp@gmail.com>
2024-09-13 20:56:43 -0500

    changes in form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,14 +1,50 @@
 import React from "react";
+import { useState } from "react";
 
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+  });
+
+  const [error, setError] = useState(false);
+  
+  const handleName = (e) => {
+    setContact({ ...contact, name: e.target.value });
+  };
+  const handleEmail = (e) => {
+    setContact({ ...contact, email: e.target.value });
+
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit", contact)
+    if (
+      contact.name.trim().length >= 6 
+    ) {
+      setError(false);
+      console.log("Gracias" + " " + contact.name + " " + "te contactaremos cuanto antes vía email")
+    } else {
+      setError(true);
+    }
+  };
 
   return (
     <div>
-      <form>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>Nombre: </label>
+      <input type="text" onChange={handleName} />
+      <label>Email: </label>
+      <input type="email" onChange={handleEmail} />
+      <button>Enviar</button>
+    </form>
+    {error ? (
+      <h4 style={{ color: "red" }}>Por favor verifique su información nuevamente</h4>
+    
+  ) :null } 
+  </div>
   );
 };
 
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,50 +1,10 @@
 import React from 'react'
-import Form from '../Components/Form'
-import { useState } from "react";
-
+import Form from "../Components/Form"
 
 const Contact = () => {
-  const [pet, setPet] = useState({
-    name: "",
-    like: "",
-  });
-
-  const [error, setError] = useState(false);
   
-  const handleName = (e) => {
-    setPet({ ...pet, name: e.target.value });
-  };
-  const handleLike = (e) => {
-    setPet({ ...pet, like: e.target.value });
-
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submit", pet)
-    if (
-      pet.name.trim().length >= 6 
-    ) {
-      setError(false);
-      console.log("Gracias" + " " + pet.name + " " + "te contactaremos cuanto antes vía email")
-    } else {
-      setError(true);
-    }
-  };
   return (
-       <div>
-      <form onSubmit={handleSubmit}>
-        <label>Nombre: </label>
-        <input type="text" onChange={handleName} />
-        <label>Email: </label>
-        <input type="email" onChange={handleLike} />
-        <button>Enviar</button>
-      </form>
-      {error ? (
-        <h4 style={{ color: "red" }}>Por favor verifique su información nuevamente</h4>
-      
-    ) :null } 
-    </div>
+   <Form></Form>
   )
 }
 
